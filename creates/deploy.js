c025e9a2ba88966aaf7785de822349606af72eb4
Sam <samfauble@gmail.com>
2020-06-23 18:06:56 -0300

    basic scaffolding of trigger Commit and create Deploy

---
+++ creates/deploy.js
@@ -0,0 +1,56 @@
+// create a particular deploy by name
+const perform = async (z, bundle) => {
+  const response = await z.request({
+    method: 'POST',
+    url: 'https://jsonplaceholder.typicode.com/posts',
+    // if `body` is an object, it'll automatically get run through JSON.stringify
+    // if you don't want to send JSON, pass a string in your chosen format here instead
+    body: {
+      name: bundle.inputData.name
+    }
+  });
+  // this should return a single object
+  return response.data;
+};
+
+module.exports = {
+  // see here for a full list of available properties:
+  // https://github.com/zapier/zapier-platform/blob/master/packages/schema/docs/build/schema.md#createschema
+  key: 'deploy',
+  noun: 'Deploy',
+
+  display: {
+    label: 'Create Deploy',
+    description: 'Creates a new deploy, probably with input from previous steps.'
+  },
+
+  operation: {
+    perform,
+
+    // `inputFields` defines the fields a user could provide
+    // Zapier will pass them in as `bundle.inputData` later. They're optional.
+    // End-users will map data into these fields. In general, they should have any fields that the API can accept. Be sure to accurately mark which fields are required!
+    inputFields: [
+      {key: 'name', required: true},
+      {key: 'fave_meal', label: 'Favorite Meal', required: false}
+    ],
+
+    // In cases where Zapier needs to show an example record to the user, but we are unable to get a live example
+    // from the API, Zapier will fallback to this hard-coded sample. It should reflect the data structure of
+    // returned records, and have obvious placeholder values that we can show to any user.
+    sample: {
+      id: 1,
+      name: 'Test'
+    },
+
+    // If fields are custom to each user (like spreadsheet columns), `outputFields` can create human labels
+    // For a more complete example of using dynamic fields see
+    // https://github.com/zapier/zapier-platform/tree/master/packages/cli#customdynamic-fields
+    // Alternatively, a static field definition can be provided, to specify labels for the fields
+    outputFields: [
+      // these are placeholders to match the example `perform` above
+      // {key: 'id', label: 'Person ID'},
+      // {key: 'name', label: 'Person Name'}
+    ]
+  }
+};
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const {
   afters = [],
 } = require('./authentication');
 
+const getCommit = require("./triggers\\commit");
+
+const createDeploy = require("./creates\\deploy");
+
 module.exports = {
   // This is just shorthand to reference the installed dependencies you have.
   // Zapier will need to know these before we can upload.
@@ -17,13 +21,17 @@ module.exports = {
   afterResponse: [...afters],
 
   // If you want your trigger to show up, you better include it here!
-  triggers: {},
+  triggers: {
+    [getCommit.key]: getCommit
+  },
 
   // If you want your searches to show up, you better include it here!
   searches: {},
 
   // If you want your creates to show up, you better include it here!
-  creates: {},
+  creates: {
+    [createDeploy.key]: createDeploy
+  },
 
   resources: {},
 };
